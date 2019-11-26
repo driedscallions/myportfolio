a3bd5d5b8dad07c3e82c96e3e8c006fe45443f68
driedscallions <coolbear2594695@gmail.com>
2019-11-26 18:26:18 +0800

    update

--- js/api.js
+++ js/api.js
@@ -1,7 +1,4 @@
-// const app = document.getElementById('root');
 
-// const logo = document.createElement('img');
-// logo.src = 'logo.png';
 
 // const container = document.createElement('div');
 // container.setAttribute('class', 'container');
@@ -11,13 +8,27 @@
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-75AC2547-F923-4961-BDBB-973353F29CD8&format=JSON', true);
+request.responseType = 'json';
+request.send();
+
+
+  // const weather = document.getElementById('weather');
+  const weatherPlace = document.getElementById('location');
+  
 
 request.onload = function () {
 
-  var data = JSON.parse(this.response);
+  var weatherContent = request.response;
   // Begin accessing JSON data here
-  console.log(data)
+  console.log(weatherContent)
+
+  
+  var weatherPlace = records.location[0].locationName;
+  for(i = 0; i < location.length; i++) { // 用迴圈跑各個縣市
+
+
+  }
+
 
 }
 
-request.send();
